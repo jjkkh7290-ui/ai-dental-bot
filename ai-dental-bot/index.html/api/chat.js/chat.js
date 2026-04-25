098ce800ed01df695bf2b36192909d014bdb4432
jjkkh7290-ui <jjkkh7290@gmail.com>
2026-04-25 07:48:45 +0300

    Add files via upload

---
+++ ai-dental-bot/index.html/api/chat.js/chat.js
@@ -0,0 +1,36 @@
+export default async function handler(req, res) {
+
+  const userMessage = req.body.message;
+
+  const response = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      model: "gpt-4o-mini",
+      messages: [
+        {
+          role: "system",
+          content: `
+You are a dental clinic assistant.
+- Be simple and friendly
+- Always suggest booking appointment
+- Do not give medical diagnosis
+`
+        },
+        {
+          role: "user",
+          content: userMessage
+        }
+      ]
+    })
+  });
+
+  const data = await response.json();
+
+  res.status(200).json({
+    reply: data.choices[0].message.content
+  });
+}
